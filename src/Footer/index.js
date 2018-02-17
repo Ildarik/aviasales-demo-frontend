1e38b3d5a381bea18ca12e3e7163d7df443be2dd
Nogmanov Ildar <nogmanov@gmail.com>
2018-02-17 09:26:02 +0300

    Style & refactor footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -9,229 +9,203 @@ import appstore from "./appstore.svg";
 import googleplay from "./googleplay.svg";
 import windowsphone from "./windowsphone.svg";
 
-const Wrapper = styled.section``;
+const Links = styled.ul`
+  list-style: none;
+  font-size: 12px;
+  padding: 0;
+`;
+
+const LinksTitle = styled.h3`
+  margin-top: 30px;
+  font-size: 12px;
+  color: #4a4a4a;
+`;
+
+const Link = styled.li`
+  margin-top: 10px;
+  color: #5b5b5c;
+`;
+
+const SocialLink = styled.a`
+  text-decoration: none;
+  font-size: 12px;
+  color: #4a4a4a;
+  margin-right: 30px;
+  margin-top: 10px;
+`;
+
+const MainLink = styled.li`
+  display: inline;
+  margin-right: 10px;
+  color: #5b5b5c;
+  line-height: 2;
+`;
+
+const SocialIcon = styled.img`
+  margin-right: 5px;
+`;
+
+const Note = styled.p`
+  margin-top: 22px;
+  font-size: 12px;
+  color: #5b5b5c;
+  text-align: center;
+`;
+
+const Copyright = styled.div`
+  font-size: 12px;
+  color: #5b5b5c;
+  margin: 20px;
+`;
 
 export default () => (
-  <Wrapper>
-    <nav>
-      <div>СТРАНЫ</div>
-      <ul>
-        <li>
-          <a href="">Россия</a>
-        </li>
-        <li>
-          <a href="">Тайланд</a>
-        </li>
-        <li>
-          <a href="">Черногория</a>
-        </li>
-        <li>
-          <a href="">Кипр</a>
-        </li>
-        <li>
-          <a href="">Болгария</a>
-        </li>
-        <li>
-          <a href="">Грузия</a>
-        </li>
-        <li>
-          <a href="">Все страны</a>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <div>ГОРОДА</div>
-      <ul>
-        <li>
-          <a href="">Москва</a>
-        </li>
-        <li>
-          <a href="">Санкт-Петербург</a>
-        </li>
-        <li>
-          <a href="">Симферополь</a>
-        </li>
-        <li>
-          <a href="">Адлер</a>
-        </li>
-        <li>
-          <a href="">Екатеринбург</a>
-        </li>
-        <li>
-          <a href="">Лондон</a>
-        </li>
-        <li>
-          <a href="">Все города</a>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <div>АВИАКОМПАНИИ</div>
-      <ul>
-        <li>
-          <a href="">Air Berlin</a>
-        </li>
-        <li>
-          <a href="">Air France</a>
-        </li>
-        <li>
-          <a href="">Alitalia</a>
-        </li>
-        <li>
-          <a href="">Air Baltic</a>
-        </li>
-        <li>
-          <a href="">Emirates</a>
-        </li>
-        <li>
-          <a href="">KLM</a>
-        </li>
-        <li>
-          <a href="">Все авиакомпании</a>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <div>АЭРОПОРТЫ</div>
-      <ul>
-        <li>
-          <a href="">Шереметьево</a>
-        </li>
-        <li>
-          <a href="">Курумоч</a>
-        </li>
-        <li>
-          <a href="">Домодедово</a>
-        </li>
-        <li>
-          <a href="">Толмачево</a>
-        </li>
-        <li>
-          <a href="">Владивосток</a>
-        </li>
-        <li>
-          <a href="">Гамбург</a>
-        </li>
-        <li>
-          <a href="">Все аэропорты</a>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <div>НАПРАВЛЕНИЯ</div>
-      <ul>
-        <li>
-          <a href="">MOW - SIP</a>
-        </li>
-        <li>
-          <a href="">MOW - AER</a>
-        </li>
-        <li>
-          <a href="">MOW - TIV</a>
-        </li>
-        <li>
-          <a href="">MOW - MRV</a>
-        </li>
-        <li>
-          <a href="">LED - MOW</a>
-        </li>
-        <li>
-          <a href="">MOW - BKK</a>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <div>СЕРВИСЫ</div>
-      <ul>
-        <li>
-          <a href="">Горящие авиабилеты</a>
-        </li>
-        <li>
-          <a href="">Календарь низких цен</a>
-        </li>
-        <li>
-          <a href="">Карта низких цен</a>
-        </li>
-        <li>
-          <a href="">Спецпредложения</a>
-        </li>
-        <li>
-          <a href="">Таблица цен</a>
-        </li>
-        <li>
-          <a href="">Блог</a>
-        </li>
-        <li>
-          <a href="">Помощь</a>
-        </li>
-      </ul>
-    </nav>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <a href="">О компании</a>
-          </li>
-          <li>
-            <a href="">Партнерская программа</a>
-          </li>
-          <li>
-            <a href="">Реклама</a>
-          </li>
-          <li>
-            <a href="">Вакансии</a>
-          </li>
-          <li>
-            <a href="">Помощь</a>
-          </li>
-          <li>
-            <a href="">Правила</a>
-          </li>
-          <li>
-            <a href="">White Label Авиабилеты</a>
-          </li>
-        </ul>
-      </nav>
-      <ul>
-        <li>
-          <a href="">
-            <img src={vk} alt="" /> Вконтакте
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={fb} alt="" />Фейсбук
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={instagram} alt="" /> Инстаграм
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={twitter} alt="" /> Твиттер
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={viber} alt="" /> Вайбер
-          </a>
-        </li>
-      </ul>
-      <div>Поиск и бронирование отелей</div>
+  <div className="container">
+    <div className="row">
+      <div className="col-xs-6">
+        <nav>
+          <LinksTitle>СТРАНЫ</LinksTitle>
+          <Links>
+            <Link>Россия</Link>
+            <Link>Тайланд</Link>
+            <Link>Черногория</Link>
+            <Link>Кипр</Link>
+            <Link>Болгария</Link>
+            <Link>Грузия</Link>
+            <Link>Все страны →</Link>
+          </Links>
+        </nav>
+      </div>
+      <div className="col-xs-6">
+        <nav>
+          <LinksTitle>ГОРОДА</LinksTitle>
+          <Links>
+            <Link>Москва</Link>
+            <Link>Санкт-Петербург</Link>
+            <Link>Симферополь</Link>
+            <Link>Адлер</Link>
+            <Link>Екатеринбург</Link>
+            <Link>Лондон</Link>
+            <Link>Все города →</Link>
+          </Links>
+        </nav>
+      </div>
+      <div className="col-xs-6">
+        <nav>
+          <Links>
+            <LinksTitle>АВИАКОМПАНИИ</LinksTitle>
+            <Link>Air Berlin</Link>
+            <Link>Air France</Link>
+            <Link>Alitalia</Link>
+            <Link>Air Baltic</Link>
+            <Link>Emirates</Link>
+            <Link>KLM</Link>
+            <Link>Все авиакомпании →</Link>
+          </Links>
+        </nav>
+      </div>
+      <div className="col-xs-6">
+        <nav>
+          <Links>
+            <LinksTitle>АЭРОПОРТЫ</LinksTitle>
+            <Link>Шереметьево</Link>
+            <Link>Курумоч</Link>
+            <Link>Домодедово</Link>
+            <Link>Толмачево</Link>
+            <Link>Владивосток</Link>
+            <Link>Гамбург</Link>
+            <Link>Все аэропорты →</Link>
+          </Links>
+        </nav>
+      </div>
+      <div className="col-xs-6">
+        <nav>
+          <Links>
+            <LinksTitle>НАПРАВЛЕНИЯ</LinksTitle>
+            <Link>MOW - SIP</Link>
+            <Link>MOW - AER</Link>
+            <Link>MOW - TIV</Link>
+            <Link>MOW - MRV</Link>
+            <Link>LED - MOW</Link>
+            <Link>MOW - BKK</Link>
+          </Links>
+        </nav>
+      </div>
+      <div className="col-xs-6">
+        <nav>
+          <LinksTitle>СЕРВИСЫ</LinksTitle>
+          <Links>
+            <Link>Горящие авиабилеты</Link>
+            <Link>Календарь низких цен</Link>
+            <Link>Карта низких цен</Link>
+            <Link>Спецредложения</Link>
+            <Link>Таблица цен</Link>
+            <Link>Блог</Link>
+            <Link>Помощь</Link>
+          </Links>
+        </nav>
+      </div>
     </div>
     <div>
-      <a>
-        <img src={appstore} alt="" />
-      </a>
-      <a>
-        <img src={googleplay} alt="" />
-      </a>
-      <a>
-        <img src={windowsphone} alt="" />
-      </a>
+      <div className="row">
+        <div className="col-xs-12">
+          <nav>
+            <Links>
+              <MainLink>О компании</MainLink>
+              <MainLink>Партнерская программа</MainLink>
+              <MainLink>Реклама</MainLink>
+              <MainLink>Вакансии</MainLink>
+              <MainLink>Помощь</MainLink>
+              <MainLink>Правила</MainLink>
+              <MainLink>White Label Авиабилеты</MainLink>
+            </Links>
+          </nav>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <SocialLink>
+            <SocialIcon src={vk} alt="" />
+            Вконтакте
+          </SocialLink>
+          <SocialLink>
+            <SocialIcon src={fb} alt="" />
+            Фейсбук
+          </SocialLink>
+          <SocialLink>
+            <SocialIcon src={instagram} alt="" />
+            Инстаграм
+          </SocialLink>
+          <SocialLink>
+            <SocialIcon src={twitter} alt="" />
+            Твиттер
+          </SocialLink>
+          <SocialLink>
+            <SocialIcon src={viber} alt="" />
+            Вайбер
+          </SocialLink>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <Note>Поиск и бронирование отелей</Note>
+        </div>
+      </div>
     </div>
-    <div>© 2007–2018, Aviasales — дешевые авиабилеты</div>
-  </Wrapper>
+    <div className="row center-xs">
+      <div className="col-xs-4">
+        <a>
+          <img src={appstore} alt="" />
+        </a>
+        <a>
+          <img src={googleplay} alt="" />
+        </a>
+        <a>
+          <img src={windowsphone} alt="" />
+        </a>
+      </div>
+    </div>
+    <div className="row center-xs">
+      <Copyright>© 2007–2018, Aviasales — дешевые авиабилеты</Copyright>
+    </div>
+  </div>
 );
